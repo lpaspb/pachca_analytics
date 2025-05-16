bcb44a525d62f110d0c5df26c0ad40ebed1698af
Павел Любачев <lpaspb@MacBook-Pro-Pavel.local>
2025-05-16 11:15:18 +0300

    chore: разрешены все домены для Vite (allowedHosts: ['all'])

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+    allowedHosts: ['all'],
     proxy: {
       '/api/pachka': {
         target: 'https://api.pachca.com',
